a04f85b6ab57f308436677360120d67e1fc546e3
Francisco Hernandez <frhernandez@csumb.edu>
2017-09-21 21:13:36 +0000

    new index

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 echo '<a href="/Labs/Lab1/h.html">Lab 1</a>';
 ?>
+<?php
+echo '<a href="/Labs/Lab2/index.html">Lab 2</a>';
+?>
+<?php
+echo '<a href="/Homework/hw1/home.html">Homework 1</a>';
+?>
